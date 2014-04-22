46533d0792ae33a39c6137041dca6fcf175ed404
Brian Boyd <brian@cubicalweb.com>
2014-04-22 19:01:12 -0400

    added join migration and revised other migrations and models

--- homework/app/models/location.rb
+++ homework/app/models/location.rb
@@ -1,16 +1,10 @@
 class Location < ActiveRecord::Base
+  has_many :students
+  has_many :courses
 
- 
+  # CITY_STATES =  ["Asheville, NC", "Atlanta, GA", "Austin, TX", "Charleston, SC", "Durham, NC", "Greenville, SC", "Houston, TX", "Spartanburg, SC", "Tampa, FL"]
 
+  # NOTE: Looking at how the CONSTANTS were added to the models, we would not need tables in the database for locations and courses. However, but doing it this way would not be good practice as an admin would not have access to add new locations and courses from an admin page.  So I will remove those CONSTANTS as we will add the list to the database via a form.
+  #   When I added those to an array format, it was just my way of taking notes.
 
-
-
-
-
-
-
-
-
-
-    CITY_STATES =  ["Asheville, NC", "Atlanta, GA", "Austin, TX", "Charleston, SC", "Durham, NC", "Greenville, SC", "Houston, TX", "Spartanburg, SC", "Tampa, FL"]
 end
--- homework/db/migrate/20140422181804_create_students.rb
+++ homework/db/migrate/20140422181804_create_students.rb
@@ -4,12 +4,11 @@ class CreateStudents < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :phone
-      t.binary :picture
+      t.attachment :avatar
       t.text :bio
       t.string :social_1
       t.string :social_2
       t.string :social_3
-      t.integer :location_id, index: true
 
       t.timestamps
     end
