6ff61cd6481623f98a333d5fda4fbb1fe74ce1ce
Brian Boyd <brian@cubicalweb.com>
2014-04-22 16:23:23 -0400

    revised migrate files and added bootstrap files

--- homework/db/migrate/20140422181804_create_students.rb
+++ homework/db/migrate/20140422181804_create_students.rb
@@ -1,6 +1,15 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.binary :picture
+      t.text :bio
+      t.string :social_1
+      t.string :social_2
+      t.string :social_3
+      t.integer :location_id, index: true
 
       t.timestamps
     end
